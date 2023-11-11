b2b643969a0f0a3320054899d8ac3ba5f14df53e
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-11 21:15:09 +0600

    Add errorpage

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <>
+            <div className="w-full h-screen flex flex-col justify-center items-center text-3xl font-black">
+                <h1>404</h1>
+                <h1>Not Found</h1>
+            </div>
+        </>
+    );
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,23 +3,54 @@ import App from "../App";
 import Home from "../pages/Home";
 import Test from "../pages/Test";
 import ServiceDetails from "../pages/ServiceDetails";
+import PrivateRoute from "./PrivateRoute";
+import LogIn from "../pages/LogIn";
+import Register from "../pages/Register";
+import About from "../pages/About";
+import Process from "../pages/Process";
+import NotFound from "../pages/NotFound";
 
 export const Router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <NotFound></NotFound>,
         children: [
             {
                 path: "/",
                 element: <Home />,
             },
             {
-                path: "/test",
-                element: <Test />,
+                path: "/login",
+                element: <LogIn />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/process",
+                element: (
+                    <PrivateRoute>
+                        <Process />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/about",
+                element: (
+                    <PrivateRoute>
+                        <About />
+                    </PrivateRoute>
+                ),
             },
             {
                 path: "/service/:id",
-                element: <ServiceDetails />,
+                element: (
+                    <PrivateRoute>
+                        <ServiceDetails />
+                    </PrivateRoute>
+                ),
             },
         ],
     },
