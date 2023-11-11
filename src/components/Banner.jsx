9092480c5dbc4e0de68486587e128f0ccae36903
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-11 12:53:43 +0600

    Add slider in banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,21 @@
+import Test from "../pages/Test";
+
 export default function Banner() {
     return (
         <>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img
+                <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+                    {/* <img
                         src="/images/stock/photo-1635805737707-575885ab0820.jpg"
                         className="max-w-sm rounded-lg shadow-2xl"
-                    />
+                    /> */}
+                    <Test />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat
-                            fugiat ut assumenda excepturi exercitationem quasi.
-                            In deleniti eaque aut repudiandae et a id nisi.
+                        <h1 className="text-5xl font-bold">Techmania</h1>
+                        <p className="py-6 md:max-w-[500px]">
+                            Organizing tech events at gigabit speed since 2020.
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        {/* <button className="btn btn-primary">Get Started</button> */}
                     </div>
                 </div>
             </div>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Test from "../pages/Test";
 
 export const Router = createBrowserRouter([
     {
         path: "/",
-        element: <App></App>,
+        element: <App />,
         children: [
             {
                 path: "/",
-                element: <Home></Home>,
+                element: <Home />,
+            },
+            {
+                path: "/test",
+                element: <Test />,
             },
         ],
     },
