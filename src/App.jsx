cb4e72152db71e513de59595d639b3317f4bd9b6
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-11 12:17:52 +0600

    Setup basic website skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 export default function App() {
     return (
-        <>
+        <div className="max-w-screen-xl mx-auto">
             <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
-        </>
+        </div>
     );
 }
